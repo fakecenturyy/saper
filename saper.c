75b549f369d08402850702eb93e4ef939a2b84c1
fakecenturyy <karpio2006@gmail.com>
2024-12-28 18:46:04 +0100

    bledy

--- saper.c
+++ saper.c
@@ -15,23 +15,14 @@ void print_usage(const char *prog_name) {
 }
 
 int main(int argc, char *argv[]) {
-    struct gracz player;
+    struct gracz player = {0, ""}; // Initialize player structure
     int a = 0, b = 0, mine_count = 0, lvl = 0;
     int opt;
 
-    while ((opt = getopt(argc, argv, "l:a:b:m:n:")) != -1) {
+    while ((opt = getopt(argc, argv, "l:n:")) != -1) {
         switch (opt) {
             case 'l':
                 lvl = atoi(optarg);
-                break;
-            case 'a':
-                a = atoi(optarg);
-                break;
-            case 'b':
-                b = atoi(optarg);
-                break;
-            case 'm':
-                mine_count = atoi(optarg);
                 break;
             case 'n':
                 snprintf(player.name, sizeof(player.name), "%s", optarg);
@@ -40,6 +31,44 @@ int main(int argc, char *argv[]) {
                 print_usage(argv[0]);
                 return EXIT_FAILURE;
         }
+    }
+
+    if (lvl == 0) {
+        printf("Wybierz poziom trudnosci \n 1-latwy    2-sredni    3-trudny    4-wlasny\n");
+        scanf("%d", &lvl);
+    }
+
+    switch (lvl) {
+        case 1:
+            a = 9;
+            b = 9;
+            mine_count = 10;
+            break;
+        case 2:
+            a = 16;
+            b = 16;
+            mine_count = 40;
+            break;
+        case 3:
+            a = 16;
+            b = 30;
+            mine_count = 99;
+            break;
+        case 4:
+            if (a == 0 || b == 0 || mine_count == 0) {
+                printf("Podaj liczbe kolumn, wierszy oraz min\n");
+                scanf("%d %d %d", &a, &b, &mine_count);
+            }
+            break;
+        default:
+            printf("Nieprawidlowy poziom trudnosci\n");
+            print_usage(argv[0]);
+            return EXIT_FAILURE;
+    }
+
+    if (player.name[0] == '\0') {
+        printf("Enter player name: ");
+        scanf("%49s", player.name);
     }
     char **rplansza=plansza_create(a,b,mine_count);
     player.score=game(rplansza,a,b,mine_count,lvl);
